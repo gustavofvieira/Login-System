ee231a5321be4c8c515e90758c6d0d1f45bf07e8
Gustavo Ferreira <gustavofvieira@live.com>
2023-04-03 15:02:58 -0300

    [main] update strong password

--- lb-api/Luiza.Labs.Domain/Validations/UserValidator.cs
+++ lb-api/Luiza.Labs.Domain/Validations/UserValidator.cs
@@ -15,9 +15,14 @@ namespace Luiza.Labs.Domain.Validations
                 .NotNull()
                 .EmailAddress();
 
-            RuleFor(x => x.Password)
-                .NotEmpty();
-                
+            RuleFor(p => p.Password).NotEmpty().WithMessage("Your password cannot be empty")
+                    .MinimumLength(8).WithMessage("Your password length must be at least 8.")
+                    .MaximumLength(16).WithMessage("Your password length must not exceed 16.")
+                    .Matches(@"[A-Z]+").WithMessage("Your password must contain at least one uppercase letter.")
+                    .Matches(@"[a-z]+").WithMessage("Your password must contain at least one lowercase letter.")
+                    .Matches(@"[0-9]+").WithMessage("Your password must contain at least one number.")
+                    .Matches(@"[!@#$%^&*()_+~`|}{[\]:;?/.,<>]+").WithMessage("Your password must contain at least one char special.");
+
         }
     }
 }
